b246d6cd4ca07d81110a0623dd3f789b126bee68
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-06-29 09:03:41 +0700

    chore: initial tutorial untill https://remix.run/docs/en/main/start/tutorial#loading-data

---
+++ tutorial/app/root.tsx
@@ -0,0 +1,89 @@
+import {
+  Form,
+  Link,
+  Links,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+  Outlet,
+  useLoaderData,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import appStyleHref from "./app.css?url";
+import { getContacts } from "./data";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: appStyleHref,
+  },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return { contacts };
+};
+
+export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>*</span> : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
