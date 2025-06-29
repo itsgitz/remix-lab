a2d5df9a5a9e1c28087066e3f9f99fc28b04090d
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-06-29 16:27:25 +0700

    feat: added favorite contact feature

--- tutorial/app/routes/contacts.$contactId.tsx
+++ tutorial/app/routes/contacts.$contactId.tsx
@@ -1,8 +1,8 @@
-import { Form, useLoaderData } from "@remix-run/react";
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Form, useFetcher, useLoaderData } from "@remix-run/react";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 import invariant from "tiny-invariant";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
@@ -12,6 +12,14 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
     throw new Response("Not found", { status: 404 });
   }
   return { contact };
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 };
 
 export default function Contact() {
@@ -77,10 +85,11 @@ export default function Contact() {
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
+  const fetcher = useFetcher();
   const favorite = contact.favorite;
 
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
         name="favorite"
@@ -88,6 +97,6 @@ const Favorite: FunctionComponent<{
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
