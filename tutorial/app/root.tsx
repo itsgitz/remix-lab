802ff2f7080d4fc818b219f8facb6141e82f6325
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-06-29 11:07:27 +0700

    chore: use NavLink

--- tutorial/app/root.tsx
+++ tutorial/app/root.tsx
@@ -1,6 +1,6 @@
 import {
   Form,
-  Link,
+  NavLink,
   Links,
   Meta,
   Scripts,
@@ -64,7 +64,12 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -73,7 +78,7 @@ export default function App() {
                         <i>No name</i>
                       )}{" "}
                       {contact.favorite ? <span>*</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
